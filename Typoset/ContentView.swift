9de14e8cccbb12a509b58fa23a6639b3fcb0c10b
doidoi00 <myjang1847@gmail.com>
2025-11-25 01:01:29 +0900

    Initial Commit

---
+++ Typoset/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Typoset
+//
+//  Created by 장민엽 on 11/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Typoset/TyposetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TyposetApp.swift
+//  Typoset
+//
+//  Created by 장민엽 on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TyposetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
